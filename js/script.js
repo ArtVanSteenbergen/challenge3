98efc40ae9fe9547171c54e09cf0a483a68ccd25
ArtVanSteenbergen <47633479+ArtVanSteenbergen@users.noreply.github.com>
2019-03-18 16:02:58 +0100

    wind visualisation

--- js/script.js
+++ js/script.js
@@ -25,10 +25,15 @@ $(document).ready(function() {
   Y = datetime.getFullYear(),
 
   requestCapeCanaveral = 'https://api.openweathermap.org/data/2.5/weather?appid=b0c8dafa512a0134e90df6ece3c2b7a2&q=cape%20canaveral&units=metric',
+  // requestCapeCanaveral = 'https://api.openweathermap.org/data/2.5/weather?appid=b0c8dafa512a0134e90df6ece3c2b7a2&q=&units=metric',
   requestBocaChica = 'https://api.openweathermap.org/data/2.5/weather?appid=b0c8dafa512a0134e90df6ece3c2b7a2&q=boca%20chica&units=metric',
   requestPortOfLosAngeles = 'https://api.openweathermap.org/data/2.5/weather?appid=b0c8dafa512a0134e90df6ece3c2b7a2&q=los%20angeles&units=metric',
 
-  //rickAndMorty = 'https://rickandmortyapi.com/api/character/',
+  capeCanaveralWindmill = TweenMax.to('#capeCanaveral .windmill', 1, {rotation: '360', ease: Linear.easeNone, repeat: -1, paused: true}),
+  bocaChicaWindmill = TweenMax.to('#bocaChica .windmill', 1, {rotation: '360', ease: Linear.easeNone, repeat: -1, paused: true}),
+  losAngelesWindmill = TweenMax.to('#losAngeles .windmill', 1, {rotation: '360', ease: Linear.easeNone, repeat: -1, paused: true}),
+
+  // rickAndMorty = 'https://rickandmortyapi.com/api/character/',
   
   capeCanaveralDirectionTween = TweenMax.to('#capeCanaveral .windDirection', 1, {rotation: '360', ease: Linear.easeNone, repeat: -1, paused: true}),
   bocaChicaDirectionTween = TweenMax.to('#bocaChica .windDirection', 1, {rotation: '360', ease: Linear.easeNone, repeat: -1, paused: true}),
@@ -118,6 +123,7 @@ TweenMax.from('header', 3, {y: '-50%', autoAlpha: 0, ease: Elastic.easeOut});
     capeCanaveral = response;
     $('#capeCanaveral .weatherIcon').attr({'src': returnIconUrl(capeCanaveral.weather[0].icon), 'alt': capeCanaveral.weather[0].description, 'title': capeCanaveral.weather[0].description});
     TweenMax.to(capeCanaveralDirectionTween, 1, {progress: capeCanaveral.wind.deg/360});
+    TweenMax.to(capeCanaveralWindmill, (15/capeCanaveral.wind.speed), {progress: 1, ease: Linear.easeNone, repeat: -1}); 
     TweenMax.to('#capeCanaveral .windSpeed', 1, {text: {value: Math.round(capeCanaveral.wind.speed*3.6)+'km/h'}});
     TweenMax.to('#capeCanaveral .cityName', 1, {text: {value: capeCanaveral.name}});
     TweenMax.fromTo('#capeCanaveral .temp',1, {text: {value: capeCanaveral.main.temp + '°C'}, autoAlpha: 0},{text: {value: capeCanaveral.main.temp + '°C'},autoAlpha:1, delay: 0.5});
@@ -134,6 +140,7 @@ TweenMax.from('header', 3, {y: '-50%', autoAlpha: 0, ease: Elastic.easeOut});
     bocaChica = response;
     $('#bocaChica .weatherIcon').attr({'src': returnIconUrl(bocaChica.weather[0].icon), 'alt': bocaChica.weather[0].description, 'title': bocaChica.weather[0].description});
     TweenMax.to(bocaChicaDirectionTween, 1, {progress: bocaChica.wind.deg/360, delay: 0.5});
+    TweenMax.to(bocaChicaWindmill, (15/bocaChica.wind.speed), {progress: 1, ease: Linear.easeNone, repeat: -1}); 
     TweenMax.to('#bocaChica .windSpeed', 1, {text: {value: Math.round(bocaChica.wind.speed*3.6)+'km/h'}, delay: 0.5});
     TweenMax.to('#bocaChica .cityName', 1, {text: {value: bocaChica.name}, delay: 0.5});
     TweenMax.fromTo('#bocaChica .temp',1, {text: {value: bocaChica.main.temp + '°C'}, autoAlpha: 0},{text: {value: bocaChica.main.temp + '°C'},autoAlpha:1, delay: 1});
@@ -150,6 +157,7 @@ TweenMax.from('header', 3, {y: '-50%', autoAlpha: 0, ease: Elastic.easeOut});
     losAngeles = response;
     $('#losAngeles .weatherIcon').attr({'src': returnIconUrl(losAngeles.weather[0].icon), 'alt': losAngeles.weather[0].description, 'title': losAngeles.weather[0].description});
     TweenMax.to(losAngelesDirectionTween, 1, {progress: losAngeles.wind.deg/360, delay: 1});
+    TweenMax.to(losAngelesWindmill, (15/losAngeles.wind.speed), {progress: 1, ease: Linear.easeNone, repeat: -1}); 
     TweenMax.to('#losAngeles .windSpeed', 1, {text: {value: Math.round(losAngeles.wind.speed*3.6)+'km/h'}, delay: 1});
     TweenMax.to('#losAngeles .cityName', 1, {text: {value: losAngeles.name}, delay: 1});
     TweenMax.fromTo('#losAngeles .temp',1, {text: {value: losAngeles.main.temp + '°C'}, autoAlpha: 0},{text: {value: losAngeles.main.temp + '°C'},autoAlpha:1, delay: 1.5});
